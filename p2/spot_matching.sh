22f99d86c9a6aee337d6862f6c6b061e8a0163e8
vvasill <pushkarev.vv14@physics.msu.ru>
2019-02-26 22:43:33 +0300

    Вт фев 26 22:43:33 MSK 2019

--- p2/spot_matching.sh
+++ p2/spot_matching.sh
@@ -13,6 +13,8 @@ factor=1.5
 
 cd ./match
 temp_outfile=../temp/temp_outfile$$
+skipped=skipped
+> $skipped
 
 for fw in $FWHM
 do
@@ -20,8 +22,8 @@ do
 	cat ../big_areas_list_equ | grep -v '^#' | while read areas_line
 	do 
 		#names def
-		str1=( $areas_line )
-		i=${str1[0]}
+		str_ba=( $areas_line )
+		i=${str_ba[0]}
 		infile_2='../def_coords/source_lists/corr_checked_big_area_sources_'$i'_'$fw
 					
 		count=0
@@ -53,11 +55,11 @@ do
 			#loop_on_spots from smica
 			cat $infile_2 | grep -v 'corr_'$fr | while read coord_line
 			do  
-				> temp_outfile
-				str2=( $coord_line )	
-				spot_num=${str2[0]}
-				ra=${str2[3]}
-				dec=${str2[4]}
+				> $temp_outfile
+				str_ssp=( $coord_line )	
+				spot_num=${str_ssp[0]}
+				ra=${str_ssp[3]}
+				dec=${str_ssp[4]}
 				
 				ra_l=$( echo $ra - $delta_small*0.5 | bc -l )
 				ra_r=$( echo $ra + $delta_small*0.5 | bc -l )
@@ -67,35 +69,38 @@ do
 				#checking, if source (from sources on big areas) is included in small_area (across spot on SMICA map)
 				awk -v ra=$ra -v dec=$dec -v ra_l=$ra_l -v ra_r=$ra_r -v dec_l=$dec_l -v dec_r=$dec_r -v spot_num=$spot_num '{if ($4 > ra_l && $4 < ra_r && $5 > dec_l && $5 < dec_r) {printf "%s %s %s %s %s\n", spot_num, $2, $3, ra, dec}}' $infile_1 >> $temp_outfile
 									
-				if [ -f $temp_outfile ]; then
-					#some output_file_modifications: merging if more than one peaks on one spot
-					cat $temp_outfile | ( 
-						t=0
-						t_err=0
-						while read temp_line
-						do
-							str3=( $temp_line )	
-							name=${str3[0]}	
-							dt=${str3[1]}
-							dt_err=${str3[2]}	
+				#some output_file_modifications: merging if more than one peaks on one spot
+				cat $temp_outfile | ( 
+					t=0
+					t_err=0
+					while read temp_line
+					do
+						str_sl=( $temp_line )	
+						name=${str_sl[0]}	
+						dt=${str_sl[1]}
+						dt_err=${str_sl[2]}	
 
-							if [ "$mode" == "S" ]
-							then	
-								### sum all peaks ###
-								t=$( echo $t + $dt | bc )
-								t_err=$( echo "sqrt($t_err*$t_err + $dt_err*$dt_err)" | bc -l )
-							fi
-							if [ "$mode" == "T" ]
-							then	
-								### find maximum ###
-								if (( $( echo $dt '>' $t | bc -l ) )); then t=$dt; fi
-								t_err=$dt_err
-							fi
+						if [ "$mode" == "S" ]
+						then	
+							### sum all peaks ###
+							t=$( echo $t + $dt | bc )
+							t_err=$( echo "sqrt($t_err*$t_err + $dt_err*$dt_err)" | bc -l )
+						fi
+						if [ "$mode" == "T" ]
+						then	
+							### find maximum ###
+							if (( $( echo $dt '>' $t | bc -l ) )); then t=$dt; fi
+							t_err=$dt_err
+						fi
 
-						done	
+					done	
+					if [ "$t" != "0" ]
+					then 
 						echo $name' '$t' '$t_err' '$ra' '$dec >> $outfile
-					)	
-				fi
+					else
+						echo $fr' '$fw' '$spot_num >> $skipped
+					fi
+				)	
 			done
 		done
 		(( count++ ))
